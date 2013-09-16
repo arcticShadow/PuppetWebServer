cf8ace8f72409b9d0c02fe9f10bfebf063734653
root <GNNA.local>
2013-09-17 10:59:35 +1200

    Added nginx support. The server is setup and run with some example vhosts. The vhosts aren't working properly as of yet (the nginx module isn't creating their documentroots).

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,14 @@
 $mynetworks = []
 include postfix
+
+node default {
+    class { 'nginx': }
+    nginx::resource::vhost { 'www.example.com':
+        ensure   => present,
+        www_root => '/var/www/www.example.com',
+    }
+    nginx::resource::vhost { 'www.isthistherealsiteoristhisjustfantasy.com':
+        ensure   => present,
+        www_root => '/var/www/www.isthistherealsiteoristhisjustfantasy.com',
+    }
+}
