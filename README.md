f306fe80d375257f088a2734a6ef464bb60acbf5
root <GNNA.local>
2013-09-30 12:37:28 +1300

    Tring to add php. vHosts with nginx need to point to the right socket, I started working on it but it's commented out for now.

--- README.md
+++ README.md
@@ -11,12 +11,21 @@ KNOWN BUGS:
 
 
 TODO:
-  + Create a New Dedicated unix user (i.e. theweb) and group
+  - Create a New Dedicated unix user (i.e. theweb) and group
   + Module https://forge.puppetlabs.com/thias/php
-  + + Configure Module for a single php-fpm pool using a dedicated user (theweb) and group
-  + + Setup fpm to listen on a socket at in /var/run/fpm/socket or similar - sockets are faster than tcp bindings (which is the default)
-  + + Obtain configuration examples and working setup from ceres - do not change anything on ceres
-  + + Seup some Defaults for PHP, for drupal. 
+    - Configure Module for a single php-fpm pool using a dedicated user (theweb) and group
+    + Setup fpm to listen on a socket at in /var/run/fpm/socket or similar - sockets are faster than tcp bindings (which is the default)
+        Need something like this put in the vhost config file:
+        location ~ \.php$ {
+                fastcgi_split_path_info ^(.+\.php)(/.+)$;
+                #NOTE: You should have "cgi.fix_pathinfo = 0;" in php.ini
+                include fastcgi_params;
+                fastcgi_param SCRIPT_FILENAME $request_filename;
+                fastcgi_intercept_errors on;
+                fastcgi_pass unix:/var/run/php5-fpm.sock;
+        }
+    + Obtain configuration examples and working setup from ceres - do not change anything on ceres
+    + Seup some Defaults for PHP, for drupal. 
   + Nginx (per vhost) Configure to pass any php files to the fpm socket (see the location tag in ceres for example, also php module has example)
   + Nginx - Add a default vhost that displays a simple page such as jupiter.netactive.co.nz does
   + Add phpMyAdmin Package
@@ -25,6 +34,6 @@ TODO:
   + Install Package - Nagios NRPE
   + Install Package - ?? 
   + cron job to optimize mysql tables nightly 6am
-  + Munin (Add to a sepearte *.pp file and include it from the main init.pp)
-  + + install and configure for appropriate software
-  + + Add Nginx Vhost
+  + Munin (Add to a sepearte .pp file and include it from the main init.pp)
+    + install and configure for appropriate software
+    + Add Nginx Vhost
--- manifests/init.pp
+++ manifests/init.pp
@@ -9,11 +9,36 @@ node default {
     nginx::resource::vhost { 'www.example.com':
         ensure   => present,
         www_root => '/var/www/www.example.com',
+        require  => File['/var/www/www.example.com'],
     }
+
+## Trying to get location stuff going    
+#     $location_config_example.com = {
+#         'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
+#         #NOTE: You should have "cgi.fix_pathinfo = 0;" in php.ini
+#         'include'                   => 'fastcgi_params',
+#         'fastcgi_param'             => 'SCRIPT_FILENAME $request_filename',
+#         'fastcgi_intercept_errors'  => 'on',
+#         'fastcgi_pass'              => 'unix:/var/run/php5-fpm.sock'
+#     }
+#     nginx::resource::location {
+#         vhost => 'www.example.com',
+#         location => '~ \.php$'
+#         location_cfg_prepend => $location_config_example.com,
+#     }
+
     nginx::resource::vhost { 'www.isthistherealsiteoristhisjustfantasy.com':
         ensure   => present,
         www_root => '/var/www/www.isthistherealsiteoristhisjustfantasy.com',
+        require  => File['/var/www/www.isthistherealsiteoristhisjustfantasy.com'],
     }
+}
+
+file { '/var/www/www.example.com': 
+    ensure => directory,
+}
+file { '/var/www/www.isthistherealsiteoristhisjustfantasy.com': 
+    ensure => directory,
 }
 
 ## Setup MySQL (will eventually be MariaDB hopefully)
@@ -41,14 +66,29 @@ class {'vim': } # This guys module is really basic, no config file even...
 
 ## Create a web user and group
 user {'webuser':
-    name => 'theweb',
+    name   => 'theweb',
     ensure => present,
-    shell => '/bin/bash',
-    gid => 'theweb',
+    shell  => '/bin/bash',
+    gid    => 'theweb',
 }
 
 group {'webusers':
-    name => 'theweb',
+    name   => 'theweb',
     ensure => present,
     before => User['webuser'],
+}
+
+## Manage php
+include php::fpm::daemon
+php::fpm::conf { 'www':
+    listen  => '/var/run/php5-fpm.sock',
+    user    => 'theweb',
+    group   => 'theweb',
+    # For the user to exist
+    require => Package['nginx'],
+}
+
+## Create a socket file for fpm
+file { '/var/run/php5-fpm.sock':
+    ensure => present,
 }
