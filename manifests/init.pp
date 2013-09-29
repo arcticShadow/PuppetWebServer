19595f9297323a70ebaefd8dce96c39f1749fe2d
root <GNNA.local>
2013-09-30 10:44:24 +1300

    Added Vim but it needs some configuration to have a nice vimrc

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,3 +20,21 @@ node default {
 class { 'mysql::server':
     config_hash => { 'root_password' => 'welcomemat' }
 }
+
+
+
+# Setup vim
+class {'vim': } # This guys module is really basic, no config file even...
+
+## I started writing this one. Needs a bit of editing to get the config right
+# package { 'vim':
+#     ensure => present,
+#     # Need to fix this to be cross-system
+#     before => File['/usr/share/vim/vim73/debian.vim'],
+# }
+# 
+# file { '/usr/share/vim/vim73/debian.vim':
+#     ensure => file,
+#     mode => 644,
+#     source => , ## GET A VIMRC FILE FROM SOMEWHERE USEFUL
+# }
